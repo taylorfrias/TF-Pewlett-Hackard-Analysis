9d60cc7cb50ac05d1d7d6d0b2c81d4037f170574
Renee Frias <TaylorFrias@Renees-MacBook-Pro.local>
2019-11-21 21:10:55 -0800

    Most recent titles

--- queries.sql
+++ queries.sql
@@ -15,13 +15,13 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- Joining departments and dept_manager tables Inner
-SELECT departments.dept_name,
-     dept_manager.emp_no,
-     dept_manager.from_date,
-     dept_manager.to_date
-FROM departments
-INNER JOIN dept_manager
-ON departments.dept_no = dept_manager.dept_no;
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
 
 -- Joining retirement_info and dept_emp tables Left
 SELECT retirement_info.emp_no,
@@ -32,5 +32,128 @@ FROM retirement_info
 LEFT JOIN dept_emp
 ON retirement_info.emp_no = dept_emp.emp_no;
 
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+ri.last_name,
+	de.to_date 
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
 
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+into dept_no_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+DROP TABLE emp_info CASCADE;
+
+-- create employee information
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no,
+	first_name,
+last_name,
+	gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT e.emp_no,
+	e.first_name,
+e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+		
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name	
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	s.from_date,
+	s.salary
+INTO title_retiring
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+
+SELECT 
+  count(ti.title)
+FROM title_retiring as ti
+GROUP BY
+  title
+HAVING count(ti.title) > 1;
+
+SELECT * FROM
+  (SELECT ti.title, count(title)
+  OVER
+    (PARTITION BY
+      first_name,
+      last_name
+    ) AS count
+  FROM title_retiring as ti) tableWithCount
+  WHERE tableWithCount.count > 1;
+  
+  SELECT DISTINCT ON (ti.tile) * FROM title_retiring as ti
+  DELETE FROM ti WHERE ti.title.id NOT IN 
+(SELECT id FROM (
+    SELECT DISTINCT ON (title) *
+  FROM title_retiring));
+
+--Create a new table that contains the following information: 
+--Employee number, First and last name, Title, from_date and to_date
 
