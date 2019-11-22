b5dfe84fe92ea28111b03c373da124a601682175
Renee Frias <TaylorFrias@Renees-MacBook-Pro.local>
2019-11-21 20:04:09 -0800

    created new tables

---
+++ queries.sql
@@ -0,0 +1,36 @@
+DROP TABLE retirement_info CASCADE;
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title varchar not null,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no));
+
+-- creat retirement_info table
+SELECT first_name, last_name, emp_no
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Joining departments and dept_manager tables Inner
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables Left
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+
+
